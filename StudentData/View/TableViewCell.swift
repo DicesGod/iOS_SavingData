a1bda403c0eb61fa0a175ddc06b8644e86777cbd
Minh Le <workonly140287@gmail.com>
2019-04-03 10:59:22 -0400

    second commit add example

---
+++ StudentData/View/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  StudentData
+//
+//  Created by Minh Le on 2019-04-03.
+//  Copyright © 2019 Minh Le. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
