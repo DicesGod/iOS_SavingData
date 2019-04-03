06731419d61e44534d34a193d00bd4a1fae888b9
Minh Le <workonly140287@gmail.com>
2019-04-03 13:46:53 -0400

    Done add,remove,update

--- StudentData/Controller/StudentViewController.swift
+++ StudentData/Controller/StudentViewController.swift
@@ -9,6 +9,24 @@
 import UIKit
 
 class StudentViewController: UIViewController {
+    
+    @IBOutlet weak var firstNameTextField: UITextField!
+    
+    @IBOutlet weak var lastNameTextField: UITextField!
+    
+    @IBAction func SaveStudent(_ sender: Any) {
+        storeStudent{
+            (done) in
+            if done{
+                navigationController?.popViewController(animated: true)
+                self.dismiss(animated: true, completion: nil)
+            }
+            else{
+                //Try to save again using new data
+            }
+        }
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,4 +45,25 @@ class StudentViewController: UIViewController {
     }
     */
 
+}
+
+extension StudentViewController{
+    func storeStudent(completion: (_ done:Bool) ->()) {
+        guard let managedContext = appDelegate?.persistentContainer.viewContext else { return }
+        let student =  Student(context: managedContext)
+        student.firstName = firstNameTextField.text
+        student.lastName = lastNameTextField.text
+        
+        do{
+            try managedContext.save()
+//            let alert = UIAlertController()
+            print("Student saved!")
+            completion(true)
+        } catch{
+            print("Failed to save student: ", error.localizedDescription)
+            completion(false)
+        }
+        
+        
+    }
 }
--- StudentData/Controller/ViewController.swift
+++ StudentData/Controller/ViewController.swift
@@ -7,14 +7,124 @@
 //
 
 import UIKit
+import CoreData
+
+let appDelegate = UIApplication.shared.delegate as? AppDelegate
+
 
 class ViewController: UIViewController {
+    
+    var students = [Student]()
+    
+    @IBOutlet weak var tableView: UITableView!
+    let cellid = "CellId"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.isHidden = true
+        
+        
+        }
+    
+    func  fetchStudents(){
+        fetchStudents{
+        (done) in
+        if done{
+            if students.count > 0 {
+                print("Data loaded! xD")
+                tableView.isHidden = false
+            }
+        } else{
+            tableView.isHidden = true
+        }
+        }
     }
-
-
+    
+    override func viewWillAppear(_ animate: Bool){
+       fetchStudents()
+        tableView.reloadData()
+    }
 }
 
+extension ViewController: UITableViewDelegate,UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return students.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellid, for: indexPath) as! TableViewCell
+        let student = students[indexPath.row]
+        cell.studentName.text = (student.firstName ?? "N/A")+" "+(student.lastName ?? "N/A")
+        if student.firstName == student.lastName{
+            cell.backgroundColor = #colorLiteral(red: 0.1411764771, green: 0.3960784376, blue: 0.5647059083, alpha: 1)
+            cell.studentName.textAlignment = .center
+            cell.studentName.textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return .none
+    }
+    
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        let removeAction = UITableViewRowAction(style: .destructive, title:"Remove"){
+            (action, indexPath) in
+            self.removeStudent(indexPath: indexPath)
+            self.fetchStudents()
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+        let updateAction = UITableViewRowAction(style: .normal, title: "Upper"){
+            (action, indexPath) in
+            self.updateStudent(indexPath: indexPath)
+            self.fetchStudents()
+            tableView.reloadRows(at: [indexPath], with: .automatic)
+        }
+        removeAction.backgroundColor = #colorLiteral(red: 0.9764705896, green: 0.850980401, blue: 0.5490196347, alpha: 1)
+        updateAction.backgroundColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)
+        return [removeAction, updateAction]
+    }
+}
+
+extension ViewController{
+    func fetchStudents(completion: (_ done: Bool) -> ()) {
+        guard let managedContext = appDelegate?.persistentContainer.viewContext else { return }
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Student")
+        //managedContext.fetch(request)
+        do{
+            students = try managedContext.fetch(request) as! [Student]
+            completion(true)
+        }catch{
+            print("Failed to fetech sutdent: ", error.localizedDescription)
+            completion(false)
+        }
+    }
+    
+    func removeStudent(indexPath: IndexPath)  {
+        guard let managedContext = appDelegate?.persistentContainer.viewContext else { return }
+        managedContext.delete(students[indexPath.row])
+        do{
+            try managedContext.save()
+        }catch{
+            print("Failed to remove student", error.localizedDescription)
+        }
+    }
+    
+    func updateStudent(indexPath: IndexPath){
+        guard let managedContext = appDelegate?.persistentContainer.viewContext else { return }
+        let student = students[indexPath.row]
+        student.lastName = student.lastName?.localizedUppercase
+        do{
+            try managedContext.save()
+        }catch{
+            print("Failed to update student:", error.localizedDescription)
+        }
+    }
+}
