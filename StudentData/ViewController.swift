7d0d36937f9c1ff81d6629497b8759faaa3ceeba
Minh Le <workonly140287@gmail.com>
2019-04-03 08:21:49 -0400

    Initial Commit

---
+++ StudentData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StudentData
+//
+//  Created by Minh Le on 2019-04-03.
+//  Copyright © 2019 Minh Le. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
